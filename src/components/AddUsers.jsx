e5e9c112023c57bbece959b4f6f1eb22cac36b79
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-12-06 17:12:28 +0500

    axios api method

--- src/components/AddUsers.jsx
+++ src/components/AddUsers.jsx
@@ -2,44 +2,71 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 const AddUsers = () => {
-    const [user,setuser] = useState()
+    const [user, setuser] = useState({
+        name: "",
+        lastname: "",
+        age: "",
+        email: ""
+    })
 
-    function handleChange(e){
-        setuser({...user,[e.target.name]: e.target.value})
+    function handleChange(e) {
+        setuser({ ...user, [e.target.name]: e.target.value })
     }
 
-    function handleSubmit(e){
-        e.preventDefault()
-        if(user){
-            axios.post("http://localhost:3000/users",user)
-            alert("submit successful!")
-            console.log(user)
+    // function handleSubmit(e){
+    //     e.preventDefault()
+    //     if(user){
+    //         axios.post("http://localhost:3000/users",user)
+    //         alert("submit successful!")
+    //         console.log(user)
+    //     }
+    // }
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!user || Object.keys(user).length === 0) {
+            alert("Please fill the form");
+            return;
         }
+
+        axios.post("http://localhost:3000/users", user)
+            .then(() => {
+                alert("Submit Successful!");
+                console.log(user);
+                setuser({
+                    name: "",
+                    lastname: "",
+                    age: "",
+                    email: ""
+                }); // clear form
+            })
+            .catch(err => console.log(err));
     }
 
-  return (
-    <>
-        <form action="" onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">First Name</label>
-                <input type="text" name="name" id="name" onChange={handleChange} />
-            </div>
-             <div>
-                <label htmlFor="lastname">Last Name</label>
-                <input type="text" name="lastname" id="lastname" onChange={handleChange} />
-            </div>
-             <div>
-                <label htmlFor="age">Age</label>
-                <input type="number" name="age" id="age" onChange={handleChange} />
-            </div>
-             <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" onChange={handleChange}/>
-            </div>
-            <button>Add User</button>
-        </form>
-    </>
-  )
+
+    return (
+        <>
+            <form action="" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">First Name</label>
+                    <input type="text" name="name" id="name" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="lastname">Last Name</label>
+                    <input type="text" name="lastname" id="lastname" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="age">Age</label>
+                    <input type="number" name="age" id="age" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" onChange={handleChange} />
+                </div>
+                <button>Add User</button>
+            </form>
+        </>
+    )
 }
 
 export default AddUsers
--- src/components/GetUsers.jsx
+++ src/components/GetUsers.jsx
@@ -2,21 +2,39 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const GetUsers = () => {
-    const [users,setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get("http://localhost:3000/users")
-        .then(res=> setUsers(res.data))
-        .catch( Error => console.log(Error))
-    },[])
-  return (
-    <div>
-        <h1>All users</h1>
-        {users.map((user)=>(
-            <h3 key={user.id}>{user.name}</h3>
-        ))}
-    </div>
-  )
+            .then(res => setUsers(res.data))
+            .catch(Error => console.log(Error))
+    }, [])
+
+    // function handleDelete(id) {
+    //     axios.delete(`http://localhost:3000/users/${id}`,{
+    //         method:"DELETE"
+    //     })
+
+    // }
+    function handleDelete(id) {
+        axios.delete(`http://localhost:3000/users/${id}`)
+            .then(() => {
+                setUsers(prev => prev.filter(user => user.id !== id));   
+            })
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <div>
+            <h1>All users</h1>
+            {[...users].reverse().map((user) => (
+                <>
+                    <h3 key={user.id}>{user.name}</h3>
+                    <button onClick={() => handleDelete(user.id)}>Delete</button>
+                </>
+            ))}
+        </div>
+    )
 }
 
 export default GetUsers
